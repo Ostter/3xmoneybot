d570c735a0d2dd6d7df3de5eba886e666e37be20
Konstantin Sharov <k.sharov@skoltech.ru>
2019-05-06 14:08:46 +0300

    add webhook

---
+++ README.md
@@ -0,0 +1,15 @@
+# 3xmoneybot
+
+Bot Telegram returns the current exchange rate to the RUB.
+
+# install
+add misc.py file with dict tokens
+***
+tokens = {
+    'telegram': '...telegram token...',
+    'currate': '...currate.ru token...'
+}
+***
+
+```bash
+python3 bot.py
---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+from flask_sslify import SSLify
+import requests
+import re
+from misc import tokens
+
+telegram_token = tokens['telegram']
+currate_token = tokens['currate']
+
+URL = 'https://api.telegram.org/bot' + telegram_token + '/'
+
+app = Flask(__name__)
+sslify = SSLify(app)
+
+
+def send_message(chat_id, text='custom text'):
+    url = URL + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': text}
+    response = requests.post(url, json=answer)
+    return response.json()
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        response = request.get_json()
+        chat_id = response['message']['chat']['id']
+        text = response['message']['text']
+
+        patternHi = '(п|П)ривет+'
+        patternBye = '(п|П)ока+'
+        if bool(re.search(patternHi, text.lower())):
+            send_message(chat_id, 'Привет! Рад тебя видеть! Чтобы получить текущий биржевой ключ валюты, напиши ее код')
+        elif bool(re.search(patternBye, text.lower())):
+            send_message(chat_id, 'Ну ты заходи если чо')
+        else:
+            rate = get_rate(text)
+            send_message(chat_id, text=rate)
+
+        return jsonify(response)
+
+    return '<h3>flask telegram bot app</h3>'
+
+
+def get_rate(currency):
+    pair = currency.upper() + 'RUB'
+    url = 'https://currate.ru/api/?get=rates&pairs=' + pair + '&key=' + currate_token
+    response = requests.get(url).json()
+    if response['status'] == 200:
+        return response['data'][pair]
+    else:
+        return 'У меня нет данных. Попробуй другой код валюты'
+
+if __name__ == '__main__':
+    app.run()
